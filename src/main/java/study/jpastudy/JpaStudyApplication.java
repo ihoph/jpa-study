947f5c78a72e9ce5545dab58d64076acb70e919a
junk-sound <wom2277@naver.com>
2020-09-02 16:46:23 +0900

    feat: jpa 2장

--- src/main/java/study/jpastudy/JpaStudyApplication.java
+++ src/main/java/study/jpastudy/JpaStudyApplication.java
@@ -1,5 +1,9 @@
 package study.jpastudy;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/study/jpastudy/chapter_2/JpaMain.java
@@ -0,0 +1,28 @@
+package study.jpastudy.chapter_2;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("study");
+        System.out.println("kkk");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            System.out.println("haha");
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
